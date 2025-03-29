13f7c9e80b476ee9a53625ad3ae7a6bb47a2055d
Ndema Emmanuel <ndemaemmanuel82@gmail.com>
2025-03-29 15:32:43 +0100

    configured cors again **

--- app.ts
+++ app.ts
@@ -13,12 +13,12 @@ const app = express();
 const PORT = process.env.PORT;
 const version = "v1";
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+// });
 app.use(express.static(path.join(__dirname, "public")));
 
 
