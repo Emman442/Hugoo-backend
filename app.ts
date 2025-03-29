1ab4c5daa7afae18428b8958a30b9735117f6481
Ndema Emmanuel <ndemaemmanuel82@gmail.com>
2025-03-29 15:27:48 +0100

    configured cors again *

--- app.ts
+++ app.ts
@@ -29,8 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 });
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-
+    origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
